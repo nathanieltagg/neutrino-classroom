6c6ac034f346ae0c8fca264540467564854fd2d9
Nathaniel Tagg <ntagg@otterbein.edu>
2014-12-12 10:51:35 -0500

    Small fix default to main page when IDing page

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
 $(function(){
   // var currentUrl = window.location.href; 
   var url = window.location.href.split('/');
-  var currentUrl =  url[url.length-1];
-  
+  var currentUrl =  url[url.length-1]; 
+  if(currentUrl=="") currentUrl="index.html";
+  console.log(currentUrl);
   var currentMenuItem = $("#cssmenu li a[href='" + currentUrl + "']");
   currentMenuItem.parents('li').addClass("active");
   // console.log("currentUrl",currentUrl);
